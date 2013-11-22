a1df92f97274ba5cc4341f6c2a7a986a54a5dca9
rjshade <robbie.shade@gmail.com>
2013-11-22 07:20:00 -0800

    Only put hostname in the prompt if (ssh && !tmux).

--- zsh/zsh.d/5_prompt.zsh
+++ zsh/zsh.d/5_prompt.zsh
@@ -3,16 +3,19 @@
 
 setopt prompt_subst
 
-# display [hostname] at start of prompt if sshed in...
+# Display hostname in prompt if using ssh, but not in tmux
+# (display hostname in tmux status line instead).
 if [[ -n "${SSH_CONNECTION}" ]]; then
+  if [[ -z "${TMUX}" ]]; then
     host_prefix='%{'$fg[yellow]'%}%m%{'$reset_color'%}'
+  fi
 fi
 
-# host_name ~/code/github/eighty-thousand-hours/ master >>>>
+# ~/code/github/eighty-thousand-hours/ master >>>>
 PROMPT='${host_prefix} %{$fg[blue]%}%4~%{$reset_color%} ${vcs_info_msg_0_}%(!.%{$fg[red]%}>.%{$fg[gray]%}>)%{$reset_color%} '
 
 # right prompt: Thu 5th Oct 18:45
-RPROMPT='%{$fg[gray]%}%D{%a %e %b %H:%M}%{$reset_color%}'
+#RPROMPT='%{$fg[gray]%}%D{%a %e %b %H:%M}%{$reset_color%}'
 
 # continuation prompt (open quote, etc.)
 PROMPT2="%{$fg[blue]%}%_%{$reset_color%} > "
