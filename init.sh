d3f68a09b61a378e3e9f270991893a1afc71bc4f
Robbie Shade <rjshade@google.com>
2018-01-24 11:01:07 -0500

    Overhaul init.sh: save backups, remove existing files

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+set -o xtrace
 
 : ${DOTFILES:?"Need to set DOTFILES to point to dotfiles repository"}
 
@@ -7,23 +12,60 @@ if [ ! -d "$DOTFILES" ]; then
   exit
 fi
 
+# Delete existing symlinks (or backup files if not symlinks)
+backup_tmp_path=/tmp/dotfiles
+mkdir -p $backup_tmp_path
+backup_dir=`mktemp -p ${backup_tmp_path} -d`
+
 cd $DOTFILES
 git submodule init
 git submodule update
 
-# Install fuzzyfinder.
-ln -s $DOTFILES/fzf ~/.fzf
-yes | ~/.fzf/install
-
-ln -s $DOTFILES/vim ~/.vim
-ln -s $DOTFILES/vim/vimrc ~/.vimrc
+vim_path=~/.vim
+if [ -e $vim_path ]; then mv $vim_path $backup_dir; fi
+ln -s $DOTFILES/vim $vim_path
+rm -rf ~/.vim-tmp
 mkdir ~/.vim-tmp
-rm -rf ~/.vim/bundle/vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -rf $vim_path/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git $vim_path/bundle/Vundle.vim
 
+vimrc_path=~/.vimrc
+if [ -e $vimrc_path ]; then mv $vimrc_path $backup_dir; fi
+ln -s $DOTFILES/vim/vimrc $vimrc_path
+vim +PluginInstall +qall
+
+mkdir -p ~/.config/nvim
+nvim_init_path=~/.config/nvim/init.vim
+if [ -e $nvim_init_path ]; then mv $nvim_init_path $backup_dir; fi
+echo -e "set runtimepath^=${vim_path} runtimepath+=${vim_path}/after\n"\
+        "let &packpath = &runtimepath\n"\
+        "source ${vimrc_path}" > $nvim_init_path
+
+fzf_path=~/.fzf
+if [ -e $fzf_path ]; then mv $fzf_path $backup_dir; fi
+ln -s $DOTFILES/fzf $fzf_path
+(yes | $fzf_path/install) || true
+
+zsh_path=~/.zsh
+if [ -e $zsh_path ]; then mv $zsh_path $backup_dir; fi
 ln -s $DOTFILES/zsh ~/.zsh
+
+zshrc_path=~/.zshrc
+if [ -e $zshrc_path ]; then mv $zshrc_path $backup_dir; fi
 ln -s $DOTFILES/zsh/zshrc ~/.zshrc
 
+tmux_conf_path=~/.tmux.conf
+if [ -e $tmux_conf_path ]; then mv $tmux_conf_path $backup_dir; fi
 ln -s $DOTFILES/tmux.conf ~/.tmux.conf
+
+gitignore_path=~/.gitignore
+if [ -e $gitignore_path ]; then mv $gitignore_path $backup_dir; fi
 ln -s $DOTFILES/gitignore ~/.gitignore
+
+gitconfig_path=~/.gitconfig
+if [ -e $gitconfig_path ]; then mv $gitconfig_path $backup_dir; fi
 ln -s $DOTFILES/gitconfig ~/.gitconfig
+
+if find "$backup_dir" -mindepth 1 -print -quit | grep -q .; then
+  echo -e "\nExisting dotfiles moved to ${backup_dir}\n\tls -a ${backup_dir}\n"
+fi
