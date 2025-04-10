28336ccb5dd994d38fa30624c71a665f3c3b3019
Robbie Shade <robbie@tryfi.com>
2025-04-10 11:50:41 -0400

    modernize neovim config a bit

--- nvim/after/plugin/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', 'ff', builtin.find_files, {}) -- search files in current directory
-vim.keymap.set('n', 'ft', builtin.git_files, {})  -- search files in current git repo
-vim.keymap.set('n', 'fg', builtin.live_grep, {})  -- search inside files
-vim.keymap.set('n', 'fb', builtin.buffers, {})    -- search open buffer file names
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+--return {
+--  {
+--    "catppuccin/nvim",
+--    name = "catppuccin",
+--    priority = 1000,
+--    config = function()
+--      vim.cmd.colorscheme("catppuccin-mocha")
+--    end,
+--  },
+--}
+
+return {
+  'deparr/tairiki.nvim',
+  lazy = false,
+  priority = 1000, -- recommended if you use tairiki as your default theme
+}
