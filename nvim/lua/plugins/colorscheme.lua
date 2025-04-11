ddf29c15f88b7eedc95b56cb3fa536fe9f9a743f
Robbie Shade <robbie@tryfi.com>
2025-04-10 21:08:13 -0400

    extract snacks config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -17,3 +17,33 @@ return {
 --    vim.cmd.colorscheme("tairiki")
 --  end,
 --}
+
+--return {
+--  'folke/tokyonight.nvim',
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    vim.cmd.colorscheme("tokyonight")
+--  end,
+--}
+
+--return {
+--  'loctvl842/monokai-pro.nvim',
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    require('monokai-pro').setup({
+--      filter = 'machine',
+--    })
+--    vim.cmd.colorscheme("monokai-pro")
+--  end,
+--}
+
+--return {
+--  'rebelot/kanagawa.nvim',
+--  lazy = false,
+--  priority = 1000,
+--  config = function()
+--    vim.cmd.colorscheme("kanagawa")
+--  end,
+--}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,49 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
+    lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  --{
-  --	  "folke/snacks.nvim",
-  --  lazy = false,
-  -- --@type snacks.Config
-  --  opts = {
-  --    input = { enabled = true },
-  --    bigfile = { enabled = true },
-  --    indent = { enabled = true },
-  --    explorer = { enabled = true },
-  --    gitbrowse = { enabled = true },
-  --  },
-  --  --opts = {
-  --  --  -- your configuration comes here
-  --  --  -- or leave it empty to use the default settings
-  --  --  -- refer to the configuration section below
-  --  --  notifier = { enabled = true },
-  --  --  quickfile = { enabled = true },
-  --  --  scope = { enabled = true },
-  --  --  statuscolumn = { enabled = true },
-  --  --  words = { enabled = true },
-  --  --},
-  --
-  -- },
-  'mhinz/vim-signify',              -- Shows git status in sidebar.
-  'tpope/vim-commentary',           -- Comment/uncomment easily.
-  'tpope/vim-fugitive',             -- Git commands in vim.
-  { import = "plugins.telescope" }, -- Fuzzy file finder.
+  'mhinz/vim-signify',     -- Shows git status in sidebar.
+  'tpope/vim-commentary',  -- Comment/uncomment easily.
+  'tpope/vim-fugitive',    -- Git commands in vim.
+  'neovim/nvim-lspconfig', -- Easy config for LSP.
+  'github/copilot.vim',    -- Github's copilot code completion.
+  'jparise/vim-graphql',   -- GraphQL syntax highlighting.
   { import = "plugins.mason" },
-  'neovim/nvim-lspconfig',          -- Easy config for LSP.
-  'github/copilot.vim',             -- Github's copilot code completion.
-  'jparise/vim-graphql',            -- GraphQL syntax highlighting.
-  'kyazdani42/nvim-web-devicons',   -- for file icons
-  'kyazdani42/nvim-tree.lua',       -- for file tree
-  { import = "plugins.colorscheme" }
+  { import = "plugins.snacks" },
+  { import = "plugins.colorscheme" },
 })
