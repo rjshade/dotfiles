daa5c2eda94a8a28d4651ada92d5eb6d2fd3cff5
Robbie Shade <robbie@tryfi.com>
2025-04-15 11:16:10 -0400

    move stuff to options

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,27 +1,2 @@
 require('core.options')
 require('core.keymaps')
-
-local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
-
--- Highlight on yank
-augroup('YankHighlight', { clear = true })
-autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '150', on_visual = true })
-  end
-})
-
-vim.diagnostic.config({
-  float = {
-    source = 'always',
-    border = 'rounded',
-  },
-})
-
-vim.o.foldenable = true
-vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldmethod = 'expr'
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -12,3 +12,21 @@ vim.opt.softtabstop = 2       -- Make spaces feel like tabs.
 vim.opt.tabstop     = 2       -- tab == 2 spaces.
 vim.opt.title       = true    -- Terminal title.
 vim.opt.undolevels  = 100000  -- Set a bigger undo history.
+
+-- Fold
+vim.o.foldenable = true
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'
+
+-- Highlight on yank
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '150', on_visual = true })
+  end
+})
