2645e023a06551ca98b8d3369a89e94d5d245736
Robbie Shade <robbie@tryfi.com>
2025-07-28 14:54:55 -0400

    tree focuse

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -14,9 +14,10 @@ return {
       },
     })
 
-    -- Open nvim-tree on startup
+    -- Open nvim-tree on startup without focus
     local function open_nvim_tree()
       require("nvim-tree.api").tree.open()
+      vim.cmd("wincmd p") -- Move focus back to previous window
     end
     vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
   end,
